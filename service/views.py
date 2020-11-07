6844bdc1bdac3ec278cb54be00eba93990398d66
Ankit <abnsd12qer@gmail.com>
2020-11-07 14:06:57 +0530

    first commit

---
+++ service/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Service,TeamMember
+# Create your views here.
+def index(request):
+    services = Service.objects.all()
+    team_members = TeamMember.objects.all()
+    context = {
+        'services': services,
+        'team_members': team_members
+    }
+
+    return render(request,'index.html',context)
